769ef9487da86c793fa35800f6b2e7f682947947
Max Ignatenko <gelraen.ua@gmail.com>
2024-01-07 14:06:58 +0000

    Fix attempt #3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://uabluerail.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/faq/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
